e94d839a7e1cae11df9b7950076249acb2003fa3
Jannis Schröder <jannis.schroeder@boehringer-ingelheim.com>
2022-01-14 15:01:02 +0100

    Added side panel to enter moves and display logged moves

--- src/main/java/_1_adaptersPackage/GameFrame.java
+++ src/main/java/_1_adaptersPackage/GameFrame.java
@@ -17,19 +17,71 @@ public class GameFrame extends JFrame {
     }
 
     public void init() {
-        this.setLayout(new GridLayout(9, 9));
-        this.setSize(new Dimension(650, 650));
+        //this.setLayout(new GridLayout(9, 9));
+        this.setSize(new Dimension(750, 650));
+        this.setLayout(new BorderLayout());
 
-        this.fillTiles();
+        JPanel mainPanel = new JPanel();
+        JPanel sidePanel = new JPanel();
+
+        this.add(mainPanel, BorderLayout.CENTER);
+        this.add(sidePanel, BorderLayout.EAST);
+
+        this.fillTiles(mainPanel);
+        this.fillSide(sidePanel);
+
 
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    private void fillTiles() {
+    private void fillSide(JPanel sidePanel){
+        sidePanel.setLayout(new BorderLayout());
+
+        JPanel sideTopPanel = new JPanel();
+        sideTopPanel.setLayout(new GridLayout(4,2));
+        sideTopPanel.setPreferredSize(new Dimension(150, 150));
+        JLabel oldRowL = new JLabel("Old row:");
+        JLabel oldColL = new JLabel("Old col:");
+        JLabel newRowL = new JLabel("New row:");
+        JLabel newColL = new JLabel("New col:");
+
+        JTextField oldRowT = new JTextField(2);
+        JTextField oldColT = new JTextField(2);
+        JTextField newRowT = new JTextField(2);
+        JTextField newColT = new JTextField(2);
+
+        sideTopPanel.add(oldRowL);
+        sideTopPanel.add(oldColL);
+        sideTopPanel.add(oldRowT);
+        sideTopPanel.add(oldColT);
+        sideTopPanel.add(newRowL);
+        sideTopPanel.add(newColL);
+        sideTopPanel.add(newRowT);
+        sideTopPanel.add(newColT);
+
+        JPanel sideCenterPanel = new JPanel();
+        sideCenterPanel.setPreferredSize(new Dimension(150, 100));
+        JButton submit = new JButton("Move");
+        //TODO: Add Actionlistener
+        sideCenterPanel.add(submit);
+
+        JPanel sideBottomPanel = new JPanel();
+        sideBottomPanel.setPreferredSize(new Dimension(150, 400));
+        JLabel loggingFrame = new JLabel("Logged Moves:");
+        sideBottomPanel.add(loggingFrame);
+
+
+        sidePanel.add(sideTopPanel, BorderLayout.NORTH);
+        sidePanel.add(sideCenterPanel, BorderLayout.CENTER);
+        sidePanel.add(sideBottomPanel, BorderLayout.SOUTH);
+    }
+
+    private void fillTiles(JPanel mainPanel) {
+        mainPanel.setLayout(new GridLayout(9, 9));
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
-                this.add(this.board.getSquareFromLocation(new Location(row, col)));
+                mainPanel.add(this.board.getSquareFromLocation(new Location(row, col)));
             }
         }
     }
